e774334439d05976a814c781196ef449ae5bc636
SantaHub <pbaustin96@gmail.com>
2017-10-22 06:44:46 +0530

    small change

--- exp5_SLP.R
+++ exp5_SLP.R
@@ -1,23 +1,39 @@
 #Use horsepower to calculate the displacement
+#setwd("~/ML/LAB_ADML_temp/data")
 
 dataset = read.csv('auto-mpg.csv')
 
+#dealing with missing values in the attribute horsepower
+dataset$horsepower = ifelse(is.na(dataset$horsepower),ave(dataset$horsepower, FUN=function(x) mean(x, na.rm=TRUE)), dataset$horsepower)
+
 #Node weights
 set.seed(1)
-weight = rnorm(7)
+weight = rnorm(398)
 
-y=dataset$displacement
-x=data.matrix(dataset[,-3])
-
+y_true = dataset$displacement
+x = dataset$horsepower
 
 #TO be used in loop
-error = rnorm(398,1)
+err = rnorm(398,1)
 
-# errorcal <- function(ar){
-#   return abs(y-ar)
-# }
+updated=0
+y_pred = x*weight
 
-while( sum(error) != 0 ) {
-  error=y-abs(x%*%weight)
-  weight=weight + (error*0.1)
+while(y_true != y_pred) {
+  y_pred = x*weight
+  
+  if(y_true<y_pred){
+    error = y_true - y_pred
+    weight=weight+error*0.1
+  }else{
+    error=y_pred-y_true
+    weight =  weight - error*0.1
+  }
+  
+  updated = updated+1
+  cat("y_pred",y_pred,"y_true",y_true,"weight",weight)
 }
+
+weight
+
+#no sigmoid function needed here. the values are direct regression.
